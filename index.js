9f7de531cf86f3feb6fd7246eadfddf2501304f5
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-19 10:05:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+customerName = "bob";
+
+upperCaseCustomerName = () => {
+  customerName = "BOB";
+};
+setBestCustomer = () => {
+  bestCustomer = "not bob";
+};
+overwriteBestCustomer = () => {
+  bestCustomer = "maybe bob";
+};
+changeLeastFavoriteCustomer = () => {
+  const leastFavouriteCustomer = "mike";
+  leastFavouriteCustomer = "john";
+};
